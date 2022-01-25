03cd5e883efb2ed241e5ec53c813af2bf2f98ddf
hutma <hutma@DESKTOP-3ENCUOP>
2022-01-24 20:09:29 -0700

    Add project files.

---
+++ Chap 4/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+    
+class AccountTest
+    {
+    static void Main()
+        {
+            //create an account object and assign it my myAccount
+            Account myAccount = new Account();
+
+            //display myAccounts' initial name (there is not one yet)
+            Console.WriteLine($"The initial name is: {myAccount.GetName()}");
+
+            //prompt for and read the name, then put the name in the object
+            Console.Write("Enter the name: ");  //prompt
+            string theName = Console.ReadLine();    //read the name
+            myAccount.SetName(theName);     //put theName in the myAccount object
+
+            //display the name stoed in the myAccount object
+            Console.WriteLine($"myAccount's name is: {myAccount.GetName()}");
+
+        }
+    }
