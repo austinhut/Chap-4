c869a8d6d6cc41715fa7cc911d6fbe6b9c9bcb0f
Austin <austin.hutmacher0113@omail.otech.edu>
2022-01-25 08:53:12 -0700

    4.6

---
+++ Chap 4/Account.cs
@@ -0,0 +1,34 @@
+﻿// Fig. 4.6: Account.cs
+// Account class that replaces public methods SetName
+// and GetName with a public Name property
+
+class Account
+{
+   private string name;
+
+   public string Name
+   {
+      get
+      {
+         return name;
+      }
+      set
+      {
+         name = value;
+      }
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Chap 4/AccountTest.cs
+++ Chap 4/AccountTest.cs
@@ -10,15 +10,15 @@ class AccountTest
             Account myAccount = new Account();
 
             //display myAccounts' initial name (there is not one yet)
-            Console.WriteLine($"The initial name is: {myAccount.GetName()}");
+            Console.WriteLine($"The initial name is: {myAccount.Name}");
 
             //prompt for and read the name, then put the name in the object
             Console.Write("Enter the name: ");  //prompt
             string theName = Console.ReadLine();    //read the name
-            myAccount.SetName(theName);     //put theName in the myAccount object
+      myAccount.Name = theName;    //put theName in the myAccount object
 
-            //display the name stoed in the myAccount object
-            Console.WriteLine($"myAccount's name is: {myAccount.GetName()}");
+      //display the name stored in the myAccount object
+      Console.WriteLine($"myAccount's name is: {myAccount.Name}");
 
         }
     }
